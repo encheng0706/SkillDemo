572f7b53476d4d1c6ff5f0407e64b6cc12407dec
encheng0706 <103291830+encheng0706@users.noreply.github.com>
2022-05-01 18:29:01 -0700

    added failing test file

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,6 @@
-asdf
+public class SkillDemo {
+  public static int DoubleVal(int i) {
+    i *= 2;
+    return i;
+  }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+  @Test
+  public void doubling(){
+    assertEquals(3, SkillDemo.DoubleVal(1));
+  }
+}
